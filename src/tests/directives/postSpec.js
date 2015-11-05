b1beb08cfc27a443256687d6e9d03f8402d09c9c
Dominick Peluso <peluso.dominick@gmail.com>
2015-11-04 20:49:57 -0500

    Add tests for directives

--- src/tests/directives/postSpec.js
+++ src/tests/directives/postSpec.js
@@ -1,12 +1,32 @@
 describe('post directive', function(){
-  beforeEach(function(){
-    module('directings');
 
-  });
-  xit("should have put the title in bold", function(){
+    var compile, scope, compiledDirective;
 
-  });
-  xit("should have prefixed the username with a 'by '", function(){
+    beforeEach(function(){
+        module('directings');
 
-  });
+        inject(function($compile, $rootScope){
+            compile = $compile;
+            scope = $rootScope.$new();
+        });
+
+        scope.post = {
+            title: "some title",
+            userName: "Dominick Peluso"
+        };
+
+        var element = angular.element('<post title="post.title" userame="post.userName"></post>');
+        compiledDirective = compile(element)(scope);
+        scope.$digest(); // processes expressions like {{ title }}
+
+    });
+
+    it("should have put the title in bold", function(){
+        var strong = compiledDirective.find('strong');
+        expect(strong.text()).toBe(scope.post.title);
+    });
+    it("should have prefixed the username with a 'by '", function(){
+        var tt = compiledDirective.find('tt');
+        expect(tt.text().substring(0,3)).toBe('by ');
+    });
 });
